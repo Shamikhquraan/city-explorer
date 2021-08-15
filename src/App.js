94d3728378e0166c7254019c2ab90d43816261c0
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-15 17:22:45 +0300

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: {},
+      searchCity: '',
+      showData: false
+    }
+  }
+
+  getLocation = async (e) => {
+    console.log('inside get location function')
+    e.preventDefault();
+
+    await this.setState({
+      searchCity: e.target.city.value
+    })
+
+    let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`;
+
+    let resultData = await axios.get(locURL);
+
+    console.log('ssssssssssss', resultData.data[0]);
+
+    this.setState({
+      cityData: resultData.data[0],
+      showData: true
+    })
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <>
+          <h2>City Explorer</h2>
+          <form onSubmit={this.getLocation}>
+            <input type='text' placeholder='Enter city' name='city' />
+            <button>submit</button>
+          </form>
+
+          {this.state.showData &&
+            <p>{this.state.searchCity} Lat:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} </p>
+          }
+
+
+        </>
+      </div>
+    )
+  }
 }
+
 
 export default App;
