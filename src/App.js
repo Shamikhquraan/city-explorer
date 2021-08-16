7e5edff1a9a37b8bc5701b7eccdd4850d62f25c7
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-16 08:42:21 +0300

    adding

--- src/App.js
+++ src/App.js
@@ -41,29 +41,28 @@ class App extends React.Component {
   render() {
     return (
      
-        <>
-          <h2>City Explorer</h2>
-          <form onSubmit={this.getLocation}>
-            <input type='text' placeholder='Enter city' name='city' />
-            <button>Explore!</button>
+        <div className="containerIMg">
+        <Form className="ImageClass" onSubmit={this.getLocation}>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>City Explore ✔ </Form.Label>
+    <Form.Control type="text" name="city" placeholder="Type here ...." />
+  </Form.Group>
+  <button type="submit" class="btn btn-primary">Explore!</button>
+</Form>
 
-          </form>
           <div>
           {this.state.showData && <p>({this.state.searchCity} Latt:{this.state.cityData.lat} /Lon:{this.state.cityData.lon} )</p>
           }
 
-<div className="img">
+         </div>
+         <div className="imgAPI">
 {this.state.showData && (
 
-<Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=11`}  /> 
+<Image className="imgClass" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=11.5`} /> 
 )}
 
 </div>
-         </div>
-
-        
-        </>
-      
+</div>    
     )
   }
 }
